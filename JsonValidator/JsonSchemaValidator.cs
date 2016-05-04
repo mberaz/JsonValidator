058b98608a7c59d1e03aee614146fbeb3871d934
michael berezin <mbearz@gmail.com>
2016-05-04 20:25:07 +0300

    added a method that get Type

--- JsonValidator/JsonSchemaValidator.cs
+++ JsonValidator/JsonSchemaValidator.cs
@@ -1,4 +1,4 @@
-
+using System;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
 using Newtonsoft.Json.Schema.Generation;
@@ -9,10 +9,13 @@ namespace JsonValidator
     {
         public static bool Validate<T> (T obj,string json) => Validate(obj,JObject.Parse(json));
 
-        public static bool Validate<T> (T obj,JObject json)
+        public static bool Validate<T> (T obj,JObject json) => Validate(obj.GetType(),json);
+        
+
+        public static bool Validate (Type type,JObject json)
         {
             JSchemaGenerator generator = new JSchemaGenerator();
-            JSchema schema = generator.Generate(typeof(T));
+            JSchema schema = generator.Generate(type);
 
             return json.IsValid(schema);
         }
